8b76db4c5c85f5f3729a9c83c5d5c7e7eca135ad
Jay Chetty <jaypaulchetty@gmail.com>
2016-12-09 11:17:02 +0000

    add task solution

--- README.md
+++ README.md
@@ -5,4 +5,4 @@ Exercise at the Edinburgh Elixir Meetup on the 7th November 2016.
 Problem: http://rosalind.info/problems/dna/
 
 - Dna.count_letters serial solution using Enum.reduce
-- Dna.count_letters_parallel parallel solution manually spawning worker processes.
+- Dna.count_letters_parallel parallel solution using tasks.
--- lib/dna.ex
+++ lib/dna.ex
@@ -2,12 +2,7 @@ defmodule Dna do
   require Logger
 
   def count_letters(dna_list) do
-    start_count = %{
-      A: 0,
-      C: 0,
-      G: 0,
-      T: 0
-    }
+    start_count = %{ A: 0, C: 0, G: 0, T: 0 }
 
     Enum.reduce(dna_list, start_count, fn(letter, count_map)->
       { _, updated_count } = Map.get_and_update( count_map, String.to_atom(letter), fn(current_count)->
@@ -17,45 +12,22 @@ defmodule Dna do
     end)
   end
 
-  def do_work do
-    receive do
-      { master_process, dna } ->
-        Logger.warn "Worker got dna #{inspect dna}"
-        count = count_letters(dna)
-        send( master_process, {self, count } )
-    end
-  end
 
   def count_letters_parallel(dna_list) do
-
     number_of_workers = 2
     size_of_chunk = length(dna_list)/number_of_workers |> round
     dna_chunks = Enum.chunk( dna_list, size_of_chunk )
 
     worker_ids = Enum.map(dna_chunks, fn(chunk)->
-      worker_id = spawn( Dna, :do_work, [])
-      send( worker_id, { self, chunk } )
-      worker_id
+      Task.async(Dna, :count_letters, [chunk])
     end)
 
-    joined_count = %{
-      A: 0,
-      C: 0,
-      G: 0,
-      T: 0
-    }
-
-    Logger.warn "About to wait for workers"
+    joined_count = %{A: 0, C: 0, G: 0, T: 0}
 
     Enum.reduce(worker_ids, joined_count, fn(worker_id, count_map)->
-      Logger.warn "About create receive block for worker #{ inspect worker_id }"
-      receive do
-        { _, result } ->
-          Logger.warn "Master got data back  #{inspect result}"
-          Map.merge(count_map, result, fn(_k, v1, v2)->  v1 + v2 end)
-      end
+      result = Task.await( worker_id )
+      Map.merge(count_map, result, fn(_k, v1, v2)->  v1 + v2 end)
     end)
-    
   end
 
 end
